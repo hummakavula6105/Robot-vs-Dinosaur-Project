7728eb1a1b2144a15ae4acec9c5e14cce2c17074
Danielle <hummakavula6105@gmail.com>
2022-05-19 20:15:00 -0400

    Importing robot & dinosaur into battlefield & beginning to write the battle

---
+++ battlefield.py
@@ -0,0 +1,14 @@
+
+from robot import Robot()
+from dinosaur import Dinosaur()
+
+
+class Battlefield:
+
+    def __init__(self):
+        self.run_game()
+
+welcome_greeting = 'Welcome one and all, for the most epic of battles!'
+print(welcome_greeting)
+
+
