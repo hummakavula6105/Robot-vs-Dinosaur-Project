4076f008f74988af3d0e2f59f42272ae015890b0
Danielle <hummakavula6105@gmail.com>
2022-05-19 20:37:53 -0400

    Adding parameters in each object, calling classes where needed to begin functions

---
+++ battle_weapon.py
@@ -0,0 +1,7 @@
+
+class Weapon:
+
+    def __init__(self):
+        self.name = 'Blaze of glory'
+        self.attack_power = 45
+        
--- battlefield.py
+++ battlefield.py
@@ -1,14 +1,19 @@
 
-from robot import Robot()
-from dinosaur import Dinosaur()
+from robot import Robot
+from dinosaur import Dinosaur
 
+
+welcome_greeting = 'Welcome one and all, for the most epic of battles!'
+print(welcome_greeting)
 
 class Battlefield:
 
     def __init__(self):
         self.run_game()
+        self.dino_attack = ({Robot.health} - 100)
+        print(f'{Robot.name} has been attacked by {Dinosaur.name}! Health level is now at {Robot.health}')
 
-welcome_greeting = 'Welcome one and all, for the most epic of battles!'
-print(welcome_greeting)
+        
+
 
 
