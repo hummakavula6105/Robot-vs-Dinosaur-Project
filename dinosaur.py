33a4aad4e29511ed900868b296a44d0e12e67897
Danielle <hummakavula6105@gmail.com>
2022-05-22 16:21:19 -0400

    Adding battle phase parameters and commands

--- dinosaur.py
+++ dinosaur.py
@@ -8,8 +8,10 @@ class Dinosaur:
         self.health = 100
         self.attack_power = 100
 
-    def print_dino_attack(self):
-        Robot.health = self.attack_power
-        print(f'{Robot.name} has been attacked by {Dinosaur.name}! Health level is now at {self.dino_attack}')
-
+    def print_dino_attack(self, health):
+        health -= self.attack_power
+        return health
     
+    
+    
+   
