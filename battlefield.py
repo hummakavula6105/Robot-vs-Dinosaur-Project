7b5164bc91259217dd00d6af4c0aadab4e8d6f9a
Danielle <hummakavula6105@gmail.com>
2022-05-23 19:07:03 -0400

    Updated attack parameters and changed attack statement from variable to string

--- battlefield.py
+++ battlefield.py
@@ -14,49 +14,37 @@ class Battlefield:
     def run_game(self):
         self.display_welcome()
         self.battle_phase()
-        # Display Winner
-        #run_game = 
-        pass
+        self.display_winner()
     
     
     def display_welcome(self):
-        print('\nWelcome one and all, for the most epic of battles!')
+        print('\nWelcome one and all, for the most epic of battles! ROBOT Gary VERSUS DINOSAUR Sparky!!!\n')
         
-    
     
     def battle_phase(self):
-        self.battle_phase_one = Battlefield()
-        
         while (self.robot_one.health > 0 and self.dinosaur_one.health > 0):
-            self.robot_one.health = self.dinosaur_one.dino_attack(self.robot_one.health)
-            self.dinosaur_one.health = self.robot_one.robo_attack(self.dinosaur_one.health)
-            time.sleep(2)
+            time.sleep(2)                     
 
-            robot_attack_message = (f'{self.robot_one.name} has been attacked by {self.dinosaur_one.name}! Health level is now at {self.robot_one.health}')
-            dinosaur_attack_message = (f'{self.dinosaur_one.name} has been attacked by {self.robot_one.name}! Health level is now at {self.dinosaur_one.health}')
+            # Sparky
+            if random.randint(0, 1) == 1:
+                self.dinosaur_one.health = self.robot_one.robo_attack(self.dinosaur_one.health)
+                print(f'\n{self.dinosaur_one.name} has been attacked by {self.robot_one.name}! Health level is now at {self.dinosaur_one.health}!\n')
+            else:
+                print(f'\n{self.robot_one.name} attempted to attack {self.dinosaur_one.name} but has missed! {self.dinosaur_one.name} health level remains at {self.dinosaur_one.health}!\n')                      
+
+            # Gary                                                                                                      #  
+            if random.randint(0, 1) == 1:
+                self.robot_one.health = self.dinosaur_one.dino_attack(self.robot_one.health)
+                print(f'\n{self.robot_one.name} has been attacked by {self.dinosaur_one.name}! Health level is now at {self.robot_one.health}!\n')
+            else: 
+                print(f'\n{self.dinosaur_one.name} tried to hit {self.robot_one.name} but missed! {self.robot_one.name} health level remains at {self.robot_one.health}!\n')
+
             
-            robot_missed_hit_message = (f'{self.robot_one.name} attempted to attack {self.dinosaur_one.name} but has missed!')
-            dinosaur_missed_hit_message = (f'{self.dinosaur_one.name} tried to hit {self.robot_one.name} but missed!')
-      
-            if random.randint(0, 1) == 1:
-                print(robot_attack_message)
-            else:
-                print(robot_missed_hit_message)        
-
-            if random.randint(0, 1) == 1:
-                print(dinosaur_attack_message)
-            else:
-                print(dinosaur_missed_hit_message)
-                
-                              
-        # print(f'{self.dinosaur_one.name} has fought valiently, but was no match for {self.robot_one.name}! {self.dinosaur_one.name} will be laid to rest.')
-        # print(f'{self.robot_one.name} has fought valiently, but was no match for {self.dinosaur_one.name}! {self.robot_one.name} will be laid to rest.')
-    
-    # def display_winner(self):
-        # while(self.robot_one > self.dinosaur_one):
-            # print (f'{self.robot_one.name} has defeated it\'s mighty competitor {self.dinosaur_one.name} and is declared the victor!')
-        # else:
-            # print(f'{self.dinosaur_one.name} has not gone extinct and is the winner of this battle!')
-
+    def display_winner(self):
+        if (self.robot_one.health > 5 and self.dinosaur_one.health < 5):
+            print (f'\n{self.dinosaur_one.name} has fought valiently, but was no match for {self.robot_one.name}! {self.dinosaur_one.name} will be laid to rest. Which means {self.robot_one.name} is declared the victor!\n')
+        elif (self.dinosaur_one.health > 5 and self.robot_one.health < 5):
+            print(f'\n{self.robot_one.name} has fought valiently, but was no match for {self.dinosaur_one.name}! {self.robot_one.name} will be laid to rest. {self.dinosaur_one.name} has not gone extinct and is the winner of this battle!\n')
+        else:
+            print (f'{self.robot_one.name} and {self.dinosaur_one.name} fought hard, to their deaths! May they both rest in peace!')
         
-  
--- robot.py
+++ robot.py
@@ -11,11 +11,3 @@ class Robot:
     def robo_attack(self, health):
         health -= self.active_weapon.attack_power
         return health
-
-
-            # Now that you have the weapon power like that, and you are returning it, what do you get when you run main.py?
-            # I haven't looked at your battlefield.py to see what other changes you've made
-            
-            # the battlefield is now a mess because I was tearing it apart to try to find the issue :(
-            # Well lets go back to it and check it out. First though, lets get rid of this extra def __init__ that you copied over for reference
-            #  : = )
